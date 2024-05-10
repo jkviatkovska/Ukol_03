e7b53c22fee0ae3ee83d99fec66540c58bc32fcc
Jana Kviatkovská <jana.kviatkovska@outlook.com>
2024-05-10 15:58:47 +0200

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('funguju!');
+const updateSkill = (skillId, value) => {
+    const skill = document.getElementById(skillId);
+    const progress = skill.querySelector('.skill__progress');
+    const skillValue = skill.querySelector('.skill__value');
+    
+    progress.style.width = `${value}%`;
+    skillValue.textContent = `${value} / 100`;
+  }
+  
+  const htmlLevel = prompt('Zadajte svoju úroveň HTML (0-100):');
+  const cssLevel = prompt('Zadajte svoju úroveň CSS (0-100):');
+  const jsLevel = prompt('Zadajte svoju úroveň JavaScriptu (0-100):');
+  
+  updateSkill('skill1', htmlLevel);
+  updateSkill('skill2', cssLevel);
+  updateSkill('skill3', jsLevel);
